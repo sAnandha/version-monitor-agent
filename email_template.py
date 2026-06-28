0d012098cdbaeeed8503fc7fb065721426c36ef4
tejabontha-code <tejabontha@gmail.com>
2026-06-28 20:31:02 +0530

    Update email_template.py

--- email_template.py
+++ email_template.py
@@ -1,187 +1,86 @@
 
 TABLE_STYLE = """
 <style>
-
 body{
     font-family:Calibri,Arial,sans-serif;
-    font-size:14px;
+    font-size:15px;
+    color:#222;
 }
-
-h2{
-    color:#1F4E79;
-}
-
 table{
     border-collapse:collapse;
     width:100%;
-    margin-bottom:30px;
+    margin:18px 0 40px 0;
 }
-
 th{
-    background:#1F4E79;
+    background:#173A5E;
     color:white;
-    border:1px solid black;
-    padding:8px;
+    border:2px solid #173A5E;
+    padding:10px;
     text-align:center;
-}
-
-td{
-    border:1px solid black;
-    padding:8px;
-}
-
-.title{
-    background:#D9EAD3;
-    color:black;
+    font-size:15px;
     font-weight:bold;
 }
-
-.center{
-    text-align:center;
+td{
+    border:1px solid #666;
+    background:#F8F8F8;
+    padding:10px;
+    vertical-align:middle;
 }
-
+.center{text-align:center;}
+.section{
+    font-size:20px;
+    font-weight:bold;
+    text-decoration:underline;
+    margin:18px 0 10px;
+}
+.comp{font-size:16px;font-weight:bold;}
 </style>
 """
 
+ICONS={
+    "Apache Tomcat":"🧩",
+    "PostgreSQL":"🐘"
+}
 
 def build_table(data, show_current=True):
-
-    headers = data["headers"]
-
-    html = f"""
-
-<h2>{data['title']}</h2>
-
-<table>
-
-<tr>
-
-<th rowspan="2">{headers['componentName']}</th>
-
-<th colspan="{len(headers['dsrVersions'])}">
-Component versions in the last release of DSR
-</th>
-
-<th rowspan="2">
-{headers['latestComponentVersion']}
-</th>
-
-<th rowspan="2">
-{headers['releaseDate']}
-</th>
-
-"""
-
+    h=data["headers"]
+    html=f'<div class="section">{data["title"]}</div>'
+    html+='<table>'
+    html+='<tr>'
+    html+=f'<th rowspan="2" width="18%">{h["componentName"]}</th>'
+    html+=f'<th colspan="{len(h["dsrVersions"])}" width="18%">Component versions in the last release of DSR</th>'
+    html+=f'<th rowspan="2" width="16%">{h["latestComponentVersion"]}</th>'
+    html+=f'<th rowspan="2" width="16%">{h["releaseDate"]}</th>'
     if show_current:
-
-        html += f"""
-<th rowspan="2">
-{headers['currentComponentVersion']}
-</th>
-"""
-
-    html += f"""
-
-<th rowspan="2">
-{headers['comments']}
-</th>
-
-</tr>
-
-<tr>
-"""
-
-    for version in headers["dsrVersions"]:
-
-        html += f"""
-<th>{version}</th>
-"""
-
-    html += "</tr>"
-
-    for component in data["components"]:
-
-        html += "<tr>"
-
-        html += f"""
-<td>{component['componentName']}</td>
-"""
-
-        for version in headers["dsrVersions"]:
-
-            html += f"""
-<td class='center'>
-{component['dsrVersions'].get(version,"-")}
-</td>
-"""
-
-        html += f"""
-
-<td class='center'>
-<b>{component['latestComponentVersion']}</b>
-</td>
-
-<td class='center'>
-{component['releaseDate']}
-</td>
-
-"""
-
+        html+=f'<th rowspan="2" width="16%">{h["currentComponentVersion"]}</th>'
+    html+=f'<th rowspan="2" width="20%">{h["comments"]}</th></tr><tr>'
+    for v in h["dsrVersions"]:
+        html+=f'<th>{v}</th>'
+    html+='</tr>'
+    for c in data["components"]:
+        icon=ICONS.get(c["componentName"],"")
+        html+='<tr>'
+        html+=f'<td class="comp">{icon} {c["componentName"]}</td>'
+        for v in h["dsrVersions"]:
+            html+=f'<td class="center">{c["dsrVersions"].get(v,"-")}</td>'
+        html+=f'<td class="center"><b>{c["latestComponentVersion"]}</b></td>'
+        html+=f'<td class="center">{c["releaseDate"]}</td>'
         if show_current:
-
-            html += f"""
-<td class='center'>
-{component['currentComponentVersion']}
-</td>
-"""
-
-        html += f"""
-<td>
-{component['comments']}
-</td>
-"""
-
-        html += "</tr>"
-
-    html += "</table>"
-
+            html+=f'<td class="center">{c["currentComponentVersion"]}</td>'
+        html+=f'<td>{c["comments"]}</td></tr>'
+    html+='</table>'
     return html
 
-
-def build_email(jdk8_data, jdk21_data):
-    html = f"""
-
+def build_email(jdk8_data,jdk21_data):
+    return f"""
 <html>
-
 {TABLE_STYLE}
-
 <body>
-
 <p>Hi Team,</p>
-
-<p>
-Please find below the version update summary for
-<b>Apache Tomcat</b> and <b>Postgres</b> components used in <b>DSR</b>.
-</p>
-
+<p>Please find below the version update summary for <b>Apache Tomcat</b> and <b>Postgres</b> components used in <b>DSR</b>.</p>
 {build_table(jdk8_data,True)}
-
-<br>
-
 {build_table(jdk21_data,False)}
-
-<p>
-
-Regards<br>
-
-<b>Version Monitor Agent</b>
-
-</p>
-
+<p>Thanks & Regards,<br><b>Version Monitor Agent</b></p>
 </body>
-
 </html>
-
 """
-
-    return html
