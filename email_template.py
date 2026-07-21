411a1cc098ea669ab0c4b1a576623501e1a9c364
S.Anandha Nivas <115523613+sAnandha@users.noreply.github.com>
2026-07-21 15:52:14 +0530

    Update email_template.py

--- email_template.py
+++ email_template.py
@@ -71,11 +71,19 @@ def build_table(data,show_current=True):
     html+='</table>'
     return html
 
-def build_email(jdk8_data,jdk21_data):
+def build_email_jdk8(jdk8_data):
     return f"""<html>{TABLE_STYLE}<body>
 <p><b>Hi Team,</b></p>
-<p>Please find below the version update summary for <b>Apache Tomcat</b> and <b>Postgres</b> components used in <b>DSR</b>.</p>
-{build_table(jdk8_data,True)}
-{build_table(jdk21_data,False)}
+<p>Please find below the version update summary for <b>JDK8</b> – <b>Apache Tomcat</b> and <b>Postgres</b> components used in <b>DSR</b>.</p>
+{build_table(jdk8_data, True)}
+<p>Thanks &amp; Regards,<br><b>Version Monitor Agent</b></p>
+</body></html>"""
+
+
+def build_email_jdk21(jdk21_data):
+    return f"""<html>{TABLE_STYLE}<body>
+<p><b>Hi Team,</b></p>
+<p>Please find below the version update summary for <b>JDK21</b> – <b>Apache Tomcat</b> and <b>Postgres</b> components used in <b>DSR</b>.</p>
+{build_table(jdk21_data, False)}
 <p>Thanks &amp; Regards,<br><b>Version Monitor Agent</b></p>
 </body></html>"""
